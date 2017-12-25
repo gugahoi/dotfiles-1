628c1c8857c8ae859e893f61c70fa1372eafb69a
Gustavo Hoirisch <gustavo.hoirisch@messagemedia.com.au>
2017-12-25 22:14:33 +1100

    configure polybar and workspace icons

--- config/polybar/scripts/current-song.sh
+++ config/polybar/scripts/current-song.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-title=$(playerctl metadata title)
-artist=$(playerctl metadata artist)
-echo "$title - $artist"
+player_status=$(playerctl status 2> /dev/null)
+if [[ $? -eq 0 ]]; then
+    metadata="$(playerctl metadata artist) - $(playerctl metadata title)"
+fi
+
+# Foreground color formatting tags are optional
+if [[ $player_status = "Playing" ]]; then
+    echo "%{F#00AA00}$metadata"       # Orange when playing
+elif [[ $player_status = "Paused" ]]; then
+    echo "%{F#65737E} $metadata"       # Greyed out info when paused
+fi
