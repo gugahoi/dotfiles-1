aa09bd6d8e228fd51806d286ff02a4f65aedd78f
Gustavo Hoirisch <gustavo.hoirisch@messagemedia.com.au>
2018-01-26 13:00:59 +1100

    kill old polybar when reloading

--- config/polybar/scripts/launch.sh
+++ config/polybar/scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 name=main
-
+pkill polybar
 if type "xrandr"; then
   for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=${monitor} polybar --reload ${name}&
